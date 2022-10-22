5399e340ad79791fb8c9c59b939f52b375d1329a
h3zt <h3zt@o2.pl>
2022-10-22 14:55:05 +0200

    html tag

---
+++ ClickSharp.Models/Data/HtmlModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClickSharp.Models.Data
+{
+    public class HtmlModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public int? ParentId { get; set; }
+        public int? ChildId { get; set; }
+        public IDictionary<string, string>? Attributes { get; set; }
+        public IDictionary<string, string>? Classes { get; set; }
+        public string? Content { get; set; }
+    }
+}
