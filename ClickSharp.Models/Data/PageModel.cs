996cbdfd4fff8843231e2816c1add7947ec028c9
h3zt <h3zt@o2.pl>
2022-11-20 21:56:44 +0100

    models upg

--- ClickSharp.Models/Data/PageModel.cs
+++ ClickSharp.Models/Data/PageModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClickSharp.Models.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ namespace ClickSharp.Models.Data
         public string Description { get; set; } = string.Empty;
         public string Url { get; set; } = string.Empty;
         public DateTime ModificationTime { get; set; } = DateTime.UtcNow;
-        public string ModifiedBy { get; set; } = string.Empty;
+        public string ModifiedBy { get; set; } = string.Empty; //rozwazyc relacje z userem?
+        public PageStatus Status { get; set; } = PageStatus.IS_DRAFT;
     }
 }
